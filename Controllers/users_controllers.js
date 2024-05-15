62e3afcb678d422b75465e2f7f159c32fe0c8f68
Miguel Angel <155111126+pirry007@users.noreply.github.com>
2024-05-15 16:41:24 -0500

    Provisional

--- Controllers/users_controllers.js
+++ Controllers/users_controllers.js
@@ -1,2 +1,31 @@
 import express from "express";
 import mongoose from "mongoose";
+
+async function create(req, res) {
+  const newUser = await User.create({
+    firstname: String,
+    lastname: String,
+    email: String,
+    password: String,
+  });
+}
+
+async function list(req, res) {
+  try {
+    const userList = await User.find();
+    res.json(userList);
+  } catch (err) {
+    res.status(500).json("Ups... Algo salio mal");
+    console.log(err);
+  }
+}
+
+async function search(req, res) {
+  try {
+    const user = await User.findById(req.params.id);
+    res.json(user);
+  } catch (err) {
+    res.status(500).json("Ups... Algo salio mal");
+    console.log(err);
+  }
+}
