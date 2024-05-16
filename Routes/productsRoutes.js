763d02aa4a79d250ca1bf43a2b130a7c8f7cc6be
Juan Pimienta <juanmapimientae@gmail.com>
2024-05-15 20:44:20 -0500

    Corrected errors

--- Routes/productsRoutes.js
+++ Routes/productsRoutes.js
@@ -1,5 +1,4 @@
 import express from "express";
-import mongoose from "mongoose";
 import productControllers from "../Controllers/product_controllers.js";
 
 const router = express.Router();
@@ -7,7 +6,8 @@ const router = express.Router();
 router.use(express.json());
 
 app.get("/api/products", productControllers.list);
-app.get("/api/products/:id", productController.find);
-app.post("/api/products", productController.create);
-app.patch("/api/products/:id", productController.update);
-app.delete("/api/products/:id", productController.destroy);
+app.get("/api/products/:id", productControllers.find);
+app.post("/api/products", productControllers.create);
+app.patch("/api/products/:id", productControllers.update);
+app.delete("/api/products/:id", productControllers.destroy);
+
