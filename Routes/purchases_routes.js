570ea0f7718e7df52f24f13af0e666795286ae8b
Juan Pimienta <juanmapimientae@gmail.com>
2024-05-16 22:59:42 -0500

    Fixed the routes

--- Routes/purchases_routes.js
+++ Routes/purchases_routes.js
@@ -3,13 +3,13 @@ import express from "express";
 
 const  purchasesRouter = express.Router();
 
-purchasesRouter.post("/purchases/:id");
-purchasesRouter.get("/purchases/:id");
-purchasesRouter.get("/user/:id");
-purchasesRouter.get("/products/:total");
-purchasesRouter.post("/purchases/:total");
-purchasesRouter.get("/purchases/:metodoPago");
-purchasesRouter.post("/purchases/:metodoPago");
+purchasesRouter.post("/api/purchases/:id");
+purchasesRouter.get("/api/purchases/:id");
+purchasesRouter.get("/api/user/:id");
+purchasesRouter.get("/api/products/:total");
+purchasesRouter.post("/api/purchases/:total");
+purchasesRouter.get("/api/purchases/:metodoPago");
+purchasesRouter.post("/api/purchases/:metodoPago");
 
 
 export default purchasesRouter;
--- server.js
+++ server.js
@@ -2,12 +2,14 @@ import "dotenv/config";
 import express from "express";
 import productsRoutes from "./Routes/productsRoutes.js";
 import userRoutes from "./Routes/users_routes.js";
+import purchasesRoutes from "./Routes/purchases_routes.js"
 
 const app = express();
 app.use(express.json());
 
 app.use("", productsRoutes);
 app.use("", userRoutes);
+app.use("", purchasesRoutes);
 
 app.listen(3000, () => {
   console.log("Servidor corriendo en puerto 3000");
