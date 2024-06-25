c7b94792222844124a855998a984e7b8efa88908
Juan Pimienta <juanmapimientae@gmail.com>
2024-06-24 20:23:13 -0500

    Made folder file as static

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ import authRoutes from "./Routes/auth_routes.js";
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
 
 app.use("", productsRoutes);
