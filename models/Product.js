91ea85e84b4db0450b75cc147f141ffc36da2fe1
JennyVall0606 <jaov669@gmail.com>
2024-07-01 10:35:50 -0500

    Correciones en funcion Update en product_controllers.js para actualizar array Size

--- models/Product.js
+++ models/Product.js
@@ -22,7 +22,10 @@ const productSchema = mongoose.Schema(
             type: [String],
             default: ["Color 1", "Color 2", "Color 3"]
         },
-        stock: Number,
+        stock: {
+            type: [String],
+            default: ["Stock 1", "Stock 2", "Stock 3"]
+        },
         image: String,
         
         
